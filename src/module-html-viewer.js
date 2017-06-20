441be92174e9801a6fd6c396fc9888390b51d259
Ye Min <yehtut.it@gmail.com>
2017-06-20 19:02:12 +0800

    fix source code paste to html view error

--- src/module-html-viewer.js
+++ src/module-html-viewer.js
@@ -18,7 +18,7 @@ export class HtmlViwer {
         let sourceView = document.getElementById('sourceView');
         if (!sourceView) {
             var txtArea = document.createElement('textarea');
-            txtArea.style.cssText = "width: 100%;margin: 0px;background: #eee;box-sizing: border-box;color: #000;font-size: 15px;outline: none;padding: 20px;line-height: 24px;font-family: Consolas, Menlo, Monaco, &quot;Courier New&quot;, monospace;position: absolute;top: 0;bottom: 0;border: none;display:none";
+            txtArea.style.cssText = "width: 100%;margin: 0px;background: #eee;box-sizing: border-box;color: #000;font-size: 15px;outline: none;padding: 20px;line-height: 24px;font-family: Consolas, Menlo, Monaco, &quot;Courier New&quot;, monospace;position: absolute;top: 0;bottom: 0;border: none;display:none;overflow-y:scroll;";
             txtArea.id = 'sourceView';
         }
         else{
@@ -29,20 +29,21 @@ export class HtmlViwer {
         let htmlEditor = quill.addContainer('ql-custom');
         htmlEditor.appendChild(txtArea);
         var myEditor = document.querySelector('#quill-editor');
+        console.log(myEditor);
 
         if (txtArea.style.display === 'none') {
-            let html = myEditor.children[0].innerHTML;
+            let html = quill.root.innerHTML;
             txtArea.value = html;
           }
 
         quill.on('text-change', (delta, oldDelta, source) => {
-          let html = myEditor.children[0].innerHTML;
+          let html = quill.root.innerHTML;
           txtArea.value = html;
         })
 
         if (txtArea.style.display === '') {
             let html = txtArea.value;
-            self.quill.pasteHTML(html);
+            this.quill.pasteHTML(html);
           }
         txtArea.style.display = txtArea.style.display === 'none' ? '' : 'none';
     }  
